3101a92706434a416c622005a2ec08f9650f47df
Yoonjin-Lee <peacemiller@konkuk.ac.kr>
2021-01-03 14:14:12 +0900

    출력 오류 해결해야함

--- B_1260.py
+++ B_1260.py
@@ -1,6 +1,35 @@
+from collections import deque
+
 n, m, v = map(int, input().split())
 
 graph = [[] for _ in range(n)]
+visited = [False] * 9
 for i in range(n):
     a, b = map(int, input().split())
     graph[a].append(b)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, v, visited)
+
+
+def bfs(gragh, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+dfs(graph, v, visited)
+
+visited = [False] * 9
+
+bfs(graph, v, visited)
